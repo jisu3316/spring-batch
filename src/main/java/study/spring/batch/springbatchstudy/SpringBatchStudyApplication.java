b038a8611361f301786f846f39d70393f30382cd
jisu3316 <jisu3316@naver.com>
2022-07-28 18:58:13 +0900

    프로젝트 생성

---
+++ src/main/java/study/spring/batch/springbatchstudy/SpringBatchStudyApplication.java
@@ -0,0 +1,14 @@
+package study.spring.batch.springbatchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBatchStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchStudyApplication.class, args);
+    }
+
+}
