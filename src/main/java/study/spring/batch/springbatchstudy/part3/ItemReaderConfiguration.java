5fcbc9ac0373100484785f5b51cff4d06971a0b6
jisu3316 <jisu3316@naver.com>
2022-07-30 23:29:50 +0900

    CSV 파일 데이터 읽기

--- src/main/java/study/spring/batch/springbatchstudy/part3/ItemReaderConfiguration.java
+++ src/main/java/study/spring/batch/springbatchstudy/part3/ItemReaderConfiguration.java
@@ -8,8 +8,13 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +29,11 @@ public class ItemReaderConfiguration {
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job itemReaderJob() {
+    public Job itemReaderJob() throws Exception {
         return this.jobBuilderFactory.get("itemReaderJob")
                 .incrementer(new RunIdIncrementer())
                 .start(this.customItemReaderStep())
+                .next(this.csvFileStep())
                 .build();
     }
 
@@ -40,6 +46,41 @@ public class ItemReaderConfiguration {
                 .build();
     }
 
+    @Bean
+    public Step csvFileStep() throws Exception {
+        return stepBuilderFactory.get("csvFileStep")
+                .<Person, Person>chunk(10)
+                .reader(this.csvFileItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private FlatFileItemReader<Person> csvFileItemReader() throws Exception {
+        DefaultLineMapper<Person> lineMapper = new DefaultLineMapper<>();
+        DelimitedLineTokenizer tokenizer = new DelimitedLineTokenizer();
+        tokenizer.setNames("id", "name", "age", "address");
+        lineMapper.setLineTokenizer(tokenizer);
+
+        lineMapper.setFieldSetMapper(fieldSet -> {
+            int id = fieldSet.readInt("id");
+            String name = fieldSet.readString("name");
+            String age = fieldSet.readString("age");
+            String address = fieldSet.readString("address");
+
+            return new Person(id, name, age, address);
+        });
+
+        FlatFileItemReader<Person> itemReader = new FlatFileItemReaderBuilder<Person>()
+                .name("csvFileItemReader")
+                .encoding("UTF-8")
+                .resource(new ClassPathResource("test.csv")) //ClassPathResource spring에서 제공하는 클래스 resources 밑의 파일을 읽을수있다.
+                .linesToSkip(1)  //test.csv를 가면 첫번째라인은 필드명을 정의해놓은거라 두번쨰 라인부터 읽겠다는 의미
+                .lineMapper(lineMapper)
+                .build();
+        itemReader.afterPropertiesSet(); //아이템리더에 필요한 필수설정값이 설정되었는지 검증메서드
+
+        return itemReader;
+    }
     private ItemWriter<? super Person> itemWriter() {
         return items -> log.info(items.stream().map(Person::getName).collect(Collectors.joining(", ")));
     }
